78a3c91acebe6759ec4619c5648e68d2129db0ad
elioclimaco <elioclimaco@gmail.com>
2019-09-19 20:37:13 -0500

    006: Inclusión de la Plantilla II

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,20 @@
 |
 */
 
+// Ruta original
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+
+/* Ruta con nombre
+Route::get('/', function($nombre){
+    return $nombre;
+})->where('nombre', '[0-9]+')->name('permiso');
+*/
+
+Route::get('/', 'InicioController@index');
 
 Route::get('/foo', function (){
     return 'Hola mundo';
